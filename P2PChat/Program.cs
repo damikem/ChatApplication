d10a08b21be45a5145a7c53d73a3778da15912f0
damikem <damikem@gmail.com>
2016-06-25 21:40:00 +0300

    now we have a working program with server client using tcp

---
+++ P2PChat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace P2PChat
+{ // State object for receiving data from remote device.
+
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var asynchronousSocketListener = new AsynchronousSocketListener();
+
+            var serverTask = new Task(() => asynchronousSocketListener.StartListening());
+
+            serverTask.Start();
+
+            var client = new Client();
+
+            client.StartClient();
+
+            Console.Read();
+            return 1;
+        }
+    }
+}
---
+++ P2PChat/StateObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace P2PChat
+{
+    public class StateObject
+    {
+        // Size of receive buffer.
+        public const int BufferSize = 256;
+        // Receive buffer.
+        public byte[] Buffer = new byte[BufferSize];
+        // Received data string.
+        public StringBuilder Sb = new StringBuilder();
+        // Client socket.
+        public Socket WorkSocket;
+    }
+}
